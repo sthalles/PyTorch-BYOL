1aa18814248e136d7b550063db8a8b9034f357d9
Thalles Silva <thalles753@gmail.com>
2020-06-19 11:26:50 -0300

    adding simclr transforms

---
+++ data/transforms.py
@@ -0,0 +1,14 @@
+from torchvision.transforms import transforms
+from data.gaussian_blur import GaussianBlur
+
+
+def get_simclr_data_transforms(input_shape, s=1):
+    # get a set of data augmentation transformations as described in the SimCLR paper.
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=eval(input_shape)[0]),
+                                          transforms.RandomHorizontalFlip(),
+                                          transforms.RandomApply([color_jitter], p=0.8),
+                                          transforms.RandomGrayscale(p=0.2),
+                                          GaussianBlur(kernel_size=int(0.1 * eval(input_shape)[0])),
+                                          transforms.ToTensor()])
+    return data_transforms
